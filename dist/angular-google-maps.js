7a836c96f2babeebee614944eab0441af44879e4
Joluma <j.mafuta@gmail.com>
2015-01-26 16:29:52 +0900

    use the 'zIndex' for the polylines

--- dist/angular-google-maps.js
+++ dist/angular-google-maps.js
@@ -6107,7 +6107,8 @@ Original idea from: http://stackoverflow.com/questions/22758950/google-map-drawi
           visible: '=',
           "static": '=',
           fit: '=',
-          events: '='
+          events: '=',
+          zIndex: '=zindex'
         };
 
         IPolyline.scopeKeys = _.keys(IPolyline.scope);
